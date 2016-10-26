524b52994c6e745f2d1050b7be5c99dec4d2b745
kba <kba@add2.de>
2016-10-26 14:33:43 +0200

    Fixed issue #1831

--- src/uiSelectSingleDirective.js
+++ src/uiSelectSingleDirective.js
@@ -1,4 +1,4 @@
-uis.directive('uiSelectSingle', ['$timeout','$compile', function($timeout, $compile) {
+uis.directive('uiSelectSingle', ['$timeout','$compile','$interpolateProvider', function($timeout, $compile, $interpolateProvider) {
   return {
     restrict: 'EA',
     require: ['^uiSelect', '^ngModel'],
@@ -65,7 +65,7 @@ uis.directive('uiSelectSingle', ['$timeout','$compile', function($timeout, $comp
       });
 
       //Idea from: https://github.com/ivaynberg/select2/blob/79b5bf6db918d7560bdd959109b7bcfb47edaf43/select2.js#L1954
-      var focusser = angular.element("<input ng-disabled='$select.disabled' class='ui-select-focusser ui-select-offscreen' type='text' id='{{ $select.focusserId }}' aria-label='{{ $select.focusserTitle }}' aria-haspopup='true' role='button' />");
+      var focusser = angular.element("<input ng-disabled='$select.disabled' class='ui-select-focusser ui-select-offscreen' type='text' id='" + $interpolateProvider.startSymbol() + " $select.focusserId " + $interpolateProvider.endSymbol() + "' aria-label='" + $interpolateProvider.startSymbol() + " $select.focusserTitle " + $interpolateProvider.endSymbol() + "' aria-haspopup='true' role='button' />");
       $compile(focusser)(scope);
       $select.focusser = focusser;
 
